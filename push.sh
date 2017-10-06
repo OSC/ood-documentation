38014d201aa91db84534214b9778bd675228b8e8
Eric Franz <efranz@osc.edu>
2017-10-06 11:48:26 -0400

    Revert "always publish documentation"
    This reverts commit 4722676a704a40c4304ebe5483140afd2f713d51.

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 GITHUB_ORG="OSC"
-GITHUB_REPO="ood-documentation-test"
+GITHUB_REPO="ood-documentation"
 NAME="Travis Builder"
 EMAIL="oscwiag@gmail.com"
 
@@ -17,26 +17,26 @@ if [[ \
       ) \
    ]]
 then
-  GITHUB_REPO="ood-documentation"
+  echo "Publishing documentation for the branch: ${TRAVIS_BRANCH}"
+
+  # Update git configuration
+  git config user.name "${NAME}"
+  git config user.email "${EMAIL}"
+
+  # Clone the gh-pages branch outside of the current repo and cd into it
+  cd ..
+  git clone -b gh-pages "https://${GITHUB_TOKEN}@github.com/${GITHUB_ORG}/${GITHUB_REPO}.git" gh-pages
+  cd gh-pages
+
+  # Clean up old build and copy in the new built html
+  rm -fr "${TRAVIS_BRANCH}"
+  cp -R ../${GITHUB_REPO}/build/html "${TRAVIS_BRANCH}"
+
+  # Add and commit changes
+  git add -A .
+  git commit -m "[ci skip] Autodoc commit for ${TRAVIS_COMMIT}."
+  # -q is very important, otherwise you leak your GITHUB_TOKEN
+  git push -q -f origin gh-pages
+else
+  echo "Not publishing documentation for commit: ${TRAVIS_COMMIT}."
 fi
-
-echo "Publishing documentation for the branch: ${TRAVIS_BRANCH} to ${GITHUB_REPO}"
-
-# Update git configuration
-git config user.name "${NAME}"
-git config user.email "${EMAIL}"
-
-# Clone the gh-pages branch outside of the current repo and cd into it
-cd ..
-git clone -b gh-pages "https://${GITHUB_TOKEN}@github.com/${GITHUB_ORG}/${GITHUB_REPO}.git" gh-pages
-cd gh-pages
-
-# Clean up old build and copy in the new built html
-rm -fr "${TRAVIS_BRANCH}"
-cp -R ../${GITHUB_REPO}/build/html "${TRAVIS_BRANCH}"
-
-# Add and commit changes
-git add -A .
-git commit -m "[ci skip] Autodoc commit for ${TRAVIS_COMMIT}."
-# -q is very important, otherwise you leak your GITHUB_TOKEN
-git push -q -f origin gh-pages
