6c4be7d39f4c3624fb76a9cb897152610eddfe0c
Kinan AlAttar <kalattar@osc.edu>
2019-08-02 02:33:43 +0000

    Update build_images.py

--- python/build_images.py
+++ python/build_images.py
@@ -6,5 +6,5 @@ string=subprocess.check_output(["find", ".", "-name", "*.drawio", "-print"])
 arr=string.splitlines()
 for s in arr:
     sD=s.decode("utf-8")
-    subprocess.run(["drawio-batch", sD, sD[22:-7] + ".png"])
+    subprocess.run(["drawio-batch", sD, 'source/images/'  + sD[22:-7] + ".png"])
 
