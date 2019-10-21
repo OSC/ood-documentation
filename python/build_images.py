9acd5db362ac4e298823d444c8ec89171bd9e774
Kinan AlAttar <kalattar@osc.edu>
2019-10-21 16:28:09 +0000

    Update for drawio-desktop commands

--- python/build_images.py
+++ python/build_images.py
@@ -7,5 +7,5 @@ string=subprocess.check_output(["find", ".", "-name", "*.drawio", "-print"])
 found_files=string.splitlines()
 for file in found_files:
     file_decoded=file.decode("utf-8")
-    subprocess.run(["drawio-batch", file_decoded, 'source/images/'  + Path(file_decoded).stem + ".png"], check=True)
+    subprocess.run(["drawio", "-x", file_decoded, "-f", "png", "-o", "source/images/" + Path(file_decoded).stem + ".png", "--no-sandbox"], check=True)
 
