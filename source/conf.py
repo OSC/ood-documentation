9a4ddfbb0e78390613177969089813a4c1b2fe64
Mario Squeo <msqueo@osc.edu>
2020-08-18 16:29:00 -0400

    Remove PNG logo. Use SVG logo

--- source/conf.py
+++ source/conf.py
@@ -180,7 +180,7 @@ html_context = {
 # of the sidebar.
 #
 # html_logo = None
-html_logo = '_static/logo.png'
+html_logo = '_static/logo.svg'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
