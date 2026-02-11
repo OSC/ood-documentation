10b42a1f5e60c2dde83fb73c2a0eaf429d9f9197
Jeff Ohrstrom <johrstrom@osc.edu>
2026-02-11 16:22:23 -0500

    this needs a trailing /

--- source/conf.py
+++ source/conf.py
@@ -156,7 +156,7 @@ html_context = {
     'current_version': release,
     'versions' : [
         ['latest', '/latest/'],
-        ['4.0', '/release-4.0'],
+        ['4.0', '/release-4.0/'],
         ['3.0', '/release-3.0/'],
         ['2.0', '/release-2.0/'],
         ['1.8', '/release-1.8/'],
